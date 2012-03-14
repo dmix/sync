546cd32d163564ff7b85a3a1f102c8747745196b
Dan McGrady <dan@dmix.ca>
2012-03-14 14:56:28 -0400

    Initial Commit

---
+++ Sync/DAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DAppDelegate.h
+//  Sync
+//
+//  Created by Dan McGrady on 3/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Sync/DAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  DAppDelegate.m
+//  Sync
+//
+//  Created by Dan McGrady on 3/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DAppDelegate.h"
+
+@implementation DAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+  // Insert code here to initialize your application
+}
+
+@end
